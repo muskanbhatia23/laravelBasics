bab9c9ddcc2e2d95bdffb329da002059470e7d0e
Muskan <muskan.officialworks.com>
2025-01-31 11:59:57 +0530

    assignment solved of updating column status to the existing table add data as active to the column status using migration

---
+++ app/Models/Student.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+//interaction with data
+class Student extends Model
+{
+    use HasFactory;
+}
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\GreetingController;
 use App\Http\Controllers\welcomeController;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
-
+use App\Models\Student;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -32,16 +32,41 @@ Route::get('/about', function () {
 
 Route::get('/hello', [GreetingController::class, 'index']);
 
-Route::get('/about', function () {
+Route::get('/students', function () {
 
 //1. using row sql queries
  //$users = DB::select('select * from users');
  //dd($users);
 
 //2. Query Builder
-$users= DB::table('users')->select(['name','email'])->whereNotNull('email')->orderBy('name')->get();
-dd($users);
+//$users= DB::table('users')->select(['name','email'])->whereNotNull('email')->orderBy('name')->get();
+//dd($users);
 
-//3. Eloquent ORM
+//3. Eloquent ORM--in models(make:model Student)
+//$stud = Student::all();
+//$stud = Student::select(['name','email'])->whereNotNull('email')->orderBy('name')->get();
+//dd($users);
+//dd($stud);
+// Find a student by ID and update status
+$student = Student::where('email','muskan@example')->first(); // Get student with ID 1
+if ($student) {
+    $student->status = 'active'; // Set status to active
+    $student->save(); // Save changes
+    echo "Student status updated successfully!";
+} else {
+    echo "Student not found!";
+}
+
+$students = Student::all();
+dd($students); // Dump & Die to see results
+/*foreach($stud as $students)
+{
+    echo $students->name."<br>";
+}
+/* adding data manually
+$student = new Student();
+$student->name= "Prince";
+$student->email= "Prince@gmail.com";
+$student->save();*/
 
 });
